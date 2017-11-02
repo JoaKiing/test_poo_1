0bbe590fa0c6316865c0a6e4fa54f9e4a4349d2d
Joakiing <juakorabanne@gmail.com>
2017-11-02 13:50:07 +0100

    se crea la clase main.java solo con el hola mundo

---
+++ Test_poo/src/main/Main.java
@@ -0,0 +1,13 @@
+
+package main;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HOLA MUNDO DESDE JAVA ");
+    }
+    
+}
