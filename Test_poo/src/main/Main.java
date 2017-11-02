784e83cd4481b00b6f21d041ae9816fb81754942
Joakiing <juakorabanne@gmail.com>
2017-11-02 14:03:50 +0100

    se creo la clase cliente y se creo un objeto llamado cliente mostrando el nombre

--- Test_poo/src/main/Main.java
+++ Test_poo/src/main/Main.java
@@ -8,6 +8,10 @@ package main;
 public class Main {
     public static void main(String[] args) {
         System.out.println("HOLA MUNDO DESDE JAVA ");
+        
+        Cliente cli =  new Cliente(1, "juako");
+        System.out.println(cli.getNombre());
+        
     }
     
 }
